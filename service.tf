66b57a0a7fffe2a9666d1bc23806e0508fb07546
Anorlondo448 <kanaend0605@gmail.com>
2018-07-14 15:58:59 +0900

    delete iam_role. It is unnecessary

--- service.tf
+++ service.tf
@@ -8,6 +8,4 @@ resource "aws_ecs_service" "fargate-demo" {
   task_definition = "${aws_ecs_task_definition.fargate-demo.arn}"
   desired_count   = 1
   launch_type     = "EC2"
-
-  # iam_role        = "${aws_iam_role.fargate-demo-service.arn}"
 }
