ba7b60b9652a81a0103189b4b8e99d96085fa103
Anorlondo448 <kanaend0605@gmail.com>
2018-07-14 15:53:06 +0900

    add iam_role for service

--- iam_role.tf
+++ iam_role.tf
@@ -32,3 +32,47 @@ resource "aws_iam_instance_profile" "fargate-demo" {
   name = "fargate-demo"
   role = "${aws_iam_role.fargate-demo.name}"
 }
+
+## for ECS service
+data "aws_iam_policy_document" "fargate-demo-service" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ecs:*",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "fargate-demo-service" {
+  name   = "fargate-demo-service"
+  policy = "${data.aws_iam_policy_document.fargate-demo-service.json}"
+}
+
+resource "aws_iam_role" "fargate-demo-service" {
+  name = "fargate-demo-service"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs.amazonaws.com"
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "fargate-demo-service" {
+  role       = "${aws_iam_role.fargate-demo-service.name}"
+  policy_arn = "${aws_iam_policy.fargate-demo-service.arn}"
+}
