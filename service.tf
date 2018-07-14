9c32b683ba02afaaf7511c1c80b8aae688721b5c
Anorlondo448 <kanaend0605@gmail.com>
2018-07-14 16:23:16 +0900

    test change

--- service.tf
+++ service.tf
@@ -7,5 +7,5 @@ resource "aws_ecs_service" "fargate-demo" {
   cluster         = "${aws_ecs_cluster.fargate-demo.id}"
   task_definition = "${aws_ecs_task_definition.fargate-demo.arn}"
   desired_count   = 1
-  launch_type     = "EC2"
+  launch_type     = "FARGATE"
 }
--- task_definition.tf
+++ task_definition.tf
@@ -6,15 +6,17 @@ resource "aws_ecs_task_definition" "fargate-demo" {
   family                = "fargate-demo"
   container_definitions = "${file("task_definitions/fargate-demo.json")}"
 
-  volume {
-    name      = "fargate-demo-storage"
-    host_path = "/ecs/fargate-demo-storage"
-  }
+  # volume {
+  #   name      = "fargate-demo-storage"
+  #   host_path = "/ecs/fargate-demo-storage"
+  # }
 
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
-  }
+  # placement_constraints {
+  #   type       = "memberOf"
+  #   expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
+  # }
+  requires_compatibilities = ["FARGATE"]
+  network_mode = "awsvpc"
 }
 
 data "template_file" "fargate-demo" {
